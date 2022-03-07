f407f370c1682af3f378718daf65782663dbbc58
ElizabethCA94 <ecarreno@unal.edu.co>
2022-03-06 23:05:55 -0500

    chore: delete a route test

--- routes/admin.php
+++ routes/admin.php
@@ -7,10 +7,4 @@ use App\Http\Controllers\Admin\ProductosController;
 
 
 Route::get('', [HomeController::class, 'index']);
-Route::get('/liz', function() {
-    return 'uello';
-})->middleware('peticion');
-
-// Route::get('productos', [ProductosController::class, 'index']);
-// // Route::resource('productos', ProductosController::class)->middleware('peticion');
 Route::resource('admin/productos', ProductosController::class)->names('admin.productos')->middleware('peticion');
